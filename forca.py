f5b1fa8f0893f81c584cc087b9bd9576665bebba
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2024-03-09 10:52:38 -0300

    Versão 1 finalizada

--- forca.py
+++ forca.py
@@ -21,3 +21,29 @@ def game():
     chances = 6
 
     letras_erradas = []
+
+    while chances > 0:
+        print(" ".join(letras_descobertas))
+        print("\n Chances restantes:", chances)
+        print("Letras erradas:", letras_erradas)
+
+        tentativa = input("\nDigite uma letra: ").lower()
+
+        if tentativa in palavra:            
+            for index, letra in enumerate(palavra):
+                if tentativa == letra:
+                    letras_descobertas[index] = letra
+                    index += 1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
+        
+        if "_" not in letras_descobertas:
+            print("\nVocê venceu, a palavra era: ", palavra)
+            break
+    if "_" in letras_descobertas:
+        print("\nVocê perdeu, a palavra era: ", palavra)
+
+if __name__ == "__main__":
+    game()
+    print("\nParabéns. Você está aprendendo programação em Python com a DSA. :)\n")
